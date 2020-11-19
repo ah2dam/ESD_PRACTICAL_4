503964675f7e5eb47820de90f92bf73acb4e2890
AuDam <Au2.Dam@live.uwe.ac.uk>
2020-11-19 15:52:42 +0000

    add week 5 activity

--- src/java/com/ServletOne.java
+++ src/java/com/ServletOne.java
@@ -8,6 +8,7 @@ package com;
 import java.io.IOException;
 import java.io.PrintWriter;
 import java.util.Random;
+import javax.servlet.RequestDispatcher;
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
@@ -33,28 +34,46 @@ public class ServletOne extends HttpServlet {
             throws ServletException, IOException {
         int numberList[] = {1,2,3,4,5,6,7,8,9,10};
         Random rdn = new Random();
-        int a = numberList[rdn.nextInt(numberList.length)];
-        int b = numberList[rdn.nextInt(numberList.length)];
-        int c = numberList[rdn.nextInt(numberList.length)];
+//        int a = numberList[rdn.nextInt(numberList.length)];
+//        int b = numberList[rdn.nextInt(numberList.length)];
+//        int c = numberList[rdn.nextInt(numberList.length)];
+        
+        String sd1 = request.getParameter("side1");
+        String sd2 = request.getParameter("side2");
+        String sd3 = request.getParameter("side3");
+        
+        int a = Integer.parseInt(sd1);
+        int b = Integer.parseInt(sd2);
+        int c = Integer.parseInt(sd3);
+        
         Triangle triangle = new Triangle(a, b, c);
+        request.setAttribute("msg",triangle.evalTriangle());
+        request.setAttribute("nums","The number are: " + a + " " + b + " " + c + "<br>" );
+        
+        RequestDispatcher rd = request.getRequestDispatcher("/viewer/TriView.jsp");
+        rd.forward(request, response);
         
         response.setContentType("text/html;charset=UTF-8");
-        try (PrintWriter out = response.getWriter()) {
-            /* TODO output your page here. You may use following sample code. */
-            out.println("<!DOCTYPE html>");
-            out.println("<html>");
-            out.println("<head>");
-            out.println("<title>Servlet ServletOne</title>");            
-            out.println("</head>");
-            out.println("<body>");
-            out.println("<h1>Servlet ServletOne at " + request.getContextPath() + "</h1>");
-            out.println("Hello this is my first application <br>");
-            out.println("The number are: " + a + " " + b + " " + c + "<br>");
-            out.println("Result: " + triangle.evalTriangle()+ "<br>");
-            out.println("<h2>OS: " + System.getProperty("os.name") + "<h2>");
-            out.println("</body>");
-            out.println("</html>");
-        }
+//        try (PrintWriter out = response.getWriter()) {
+//            /* TODO output your page here. You may use following sample code. */
+//            out.println("<!DOCTYPE html>");
+//            out.println("<html>");
+//            out.println("<head>");
+//            out.println("<title>Servlet ServletOne</title>");
+//            out.println("</head>");
+//            out.println("<body>");
+//            out.println("<h1>Servlet ServletOne at " + request.getContextPath() + "</h1>");
+//            out.println("Hello this is my first application <br>");
+//            //Histogram
+//
+//            //Triangle
+//            out.println("The number are: " + a + " " + b + " " + c + "<br>");
+//            out.println("Result: " + triangle.evalTriangle()+ "<br>");
+//            out.println("<h2>OS: " + System.getProperty("os.name") + "<h2>");
+//            out.println("<h2>Computer user name: " + System.getenv("USERNAME") + "<h2>");
+//            out.println("</body>");
+//            out.println("</html>");
+//        }
     }
 
     // <editor-fold defaultstate="collapsed" desc="HttpServlet methods. Click on the + sign on the left to edit the code.">
---
+++ web/viewer/TriView.jsp
@@ -0,0 +1,25 @@
+<%-- 
+    Document   : TriView
+    Created on : 19-Nov-2020, 14:51:13
+    Author     : zZMerciZz
+--%>
+
+<%@page contentType="text/html" pageEncoding="UTF-8"%>
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
+        <title>Tri-view Page</title>
+    </head>
+    <body>
+        <h1>The result:</h1>
+        
+        <%=request.getAttribute("nums")%>
+        
+        <h2>
+            <%= String str = (String)request.getAttribute("msg");
+                out.println(str);
+            %>            
+        </h2>
+    </body>
+</html>
